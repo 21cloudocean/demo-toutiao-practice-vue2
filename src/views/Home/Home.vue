2c2cd7014e1af29c0d2151a2a473a824d805f896
21cloudocean <437508630@qq.com>
2022-08-01 22:54:45 +0800

    initArticleList

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -6,8 +6,35 @@
 </template>
 
 <script>
+// import request.js
+import request from '@/utils/request.js'
 export default {
-  name: 'Home'
+  name: 'Home',
+  data() {
+    return {
+      // 页码值
+      page: 1,
+      // 每页有多少数据
+      limit: 10
+    }
+  },
+  created() {
+    this.initArticleList()
+  },
+  methods: {
+    // 封装获取列表数据的方法
+    async initArticleList() {
+      // 发起GET请求，获取文章的列表数据
+      const { data: res } = await request.get('/articles', {
+        // 请求参数
+        params: {
+          _page: this.page,
+          _limit: this.limit
+        }
+      })
+      console.log(res)
+    }
+  }
 }
 </script>
 
